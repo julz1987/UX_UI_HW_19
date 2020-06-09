d5aaa8d19d500d3b44c39bbd29a63cee6d8238b0
julz1987 <65084731+julz1987@users.noreply.github.com>
2020-06-09 13:04:24 -0400

    interaction elements

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function() {
+  let button = $('.readMore');
+  button.on('mouseenter', function(event) {
+    
+    let bOffset = $(this).offset();
+    let rX = event.pageX - bOffset.left;
+    let rY = event.pageY - bOffset.top;
+
+    $(this).find('.circle').css({
+      top: rY,
+      left: rX
+    });
+
+  })
+
+  button.on('mouseout', function(event) {
+    
+    let bOffset = $(this).offset();
+    let rX = event.pageX - bOffset.left;
+    let rY = event.pageY - bOffset.top;
+
+    $(this).find('.circle').css({
+      top: rY,
+      left: rX
+    });
+    
+  })
+})
